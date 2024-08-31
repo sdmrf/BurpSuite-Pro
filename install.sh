d41a1dc46711aabfc9db56f6da632883a46fed4e
Sdmrf <syedmarufali23@gmail.com>
2024-08-31 17:23:07 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define constants
 REPO_URL="https://github.com/sdmrf/BurpSuite-Pro.git"
 BURP_DIR="/usr/share/burpsuitepro"
 BURP_CLONE_DIR="$HOME/BurpSuite-Pro"
@@ -8,49 +7,41 @@ BURP_SCRIPT="/usr/local/bin/burpsuitepro"
 BURP_RELEASES_URL="https://portswigger.net/burp/releases"
 LOADER_JAR="BurpLoaderKeyGen.jar"
 
-# Function to print status messages
 function print_status() {
     echo -e "\e[1;34m$1\e[0m"
 }
 
-# Function to download the latest Burp Suite Professional version
 function download_burpsuite() {
     print_status "Downloading Burpsuite Professional Latest..."
     local html
     html=$(curl -s "$BURP_RELEASES_URL")
     local version
     version=$(echo "$html" | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
-    local download_link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
+    local download_link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=$version"
     print_status "Found Burpsuite Version: $version"
-    wget "$download_link" -O "burpsuite_pro_v$version.jar" --quiet
+    wget "$download_link" -O "$BURP_DIR/burpsuite_pro_v$version.jar" --quiet
     echo "$version"
 }
 
-# Clean up existing directory if it exists
 if [ -d "$BURP_CLONE_DIR" ]; then
     print_status "Cleaning up existing directory $BURP_CLONE_DIR..."
     rm -rf "$BURP_CLONE_DIR"
 fi
 
-# Clone the repository into $HOME/BurpSuite-Pro
 print_status "Cloning Sdmrf Burpsuite Professional..."
 git clone "$REPO_URL" "$BURP_CLONE_DIR" || { echo "Cloning failed!"; exit 1; }
 cd "$BURP_CLONE_DIR" || { echo "Cannot navigate to Burpsuite-Pro directory!"; exit 1; }
 
-# Set up Burp Suite directory and copy loader
 print_status "Setting up Burpsuite Professional..."
 sudo mkdir -p "$BURP_DIR"
 sudo cp "$LOADER_JAR" "$BURP_DIR" || { echo "Failed to copy $LOADER_JAR!"; exit 1; }
 cd "$BURP_DIR" || { echo "Cannot navigate to Burpsuite directory!"; exit 1; }
 
-# Download the latest Burp Suite Professional and capture version
 version=$(download_burpsuite)
 
-# Start the Key Generator
 print_status "Starting Key Generator..."
 (java -jar "$BURP_DIR/$LOADER_JAR") & sleep 2
 
-# Generate executable script for Burp Suite, including the captured version
 print_status "Generating executable script for Burpsuite Professional..."
 cat << EOF | sudo tee "$BURP_SCRIPT" > /dev/null
 #!/bin/bash
@@ -65,10 +56,8 @@ java \\
   -jar $BURP_DIR/burpsuite_pro_v$version.jar &
 EOF
 
-# Make the script executable
 sudo chmod +x "$BURP_SCRIPT"
 print_status "Script generated at $BURP_SCRIPT"
 
-# Execute Burp Suite Professional
 print_status "Launching Burpsuite Professional..."
 $BURP_SCRIPT
