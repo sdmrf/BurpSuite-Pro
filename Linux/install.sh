759cad5d8b4dbebc855a403ecf79012ddb692d66
sdmrf <syedmarufali23@gmail.com>
2024-08-31 23:06:26 +0530

    Updated

--- Linux/install.sh
+++ Linux/install.sh
@@ -2,10 +2,10 @@
 
 REPO_URL="https://github.com/sdmrf/BurpSuite-Pro.git"
 BURP_DIR="/usr/share/burpsuitepro"
-BURP_CLONE_DIR="$HOME/BurpSuite-Pro"  # Use $HOME for user's home directory
+BURP_CLONE_DIR="$HOME/BurpSuite-Pro"
 BURP_SCRIPT="/usr/local/bin/burpsuitepro"
 BURP_RELEASES_URL="https://portswigger.net/burp/releases"
-LOADER_JAR="BurpLoaderKeyGen.jar"
+LOADER_JAR_URL="https://raw.githubusercontent.com/sdmrf/BurpSuiteLoaderGen/main/BurpLoaderKeyGen.jar"
 
 print_status() {
     echo -e "\e[1;34m$1\e[0m"
@@ -20,7 +20,7 @@ download_burpsuite() {
     version=$(echo "$html" | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     download_link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
 
-    wget "$download_link" -O "$BURP_DIR/burpsuite_pro_v$version.jar" -q --progress=bar:force || { echo "Download failed!"; exit 1; }
+    wget "$download_link" -O "$BURP_DIR/burpsuite_pro.jar" -q --progress=bar:force || { echo "Download failed!"; exit 1; }
     print_status "Downloaded Burp Suite Version: $version"
 }
 
@@ -39,7 +39,7 @@ clone_repo() {
 setup_burpsuite() {
     print_status "Setting up Burp Suite Professional..."
     sudo mkdir -p "$BURP_DIR" || { echo "Failed to create directory!"; exit 1; }
-    sudo cp "$BURP_CLONE_DIR/$LOADER_JAR" "$BURP_DIR" || { echo "Failed to copy $LOADER_JAR!"; exit 1; }
+    wget "$LOADER_JAR_URL" -O "$BURP_DIR/BurpLoaderKeyGen.jar" || { echo "Failed to download BurpLoaderKeyGen.jar!"; exit 1; }
 }
 
 generate_script() {
@@ -52,9 +52,9 @@ java \\
   --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED \\
   --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED \\
   --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED \\
-  -javaagent:$BURP_DIR/$LOADER_JAR \\
+  -javaagent:$BURP_DIR/BurpLoaderKeyGen.jar \\
   -noverify \\
-  -jar $BURP_DIR/burpsuite_pro_v$version.jar &
+  -jar $BURP_DIR/burpsuite_pro.jar &
 EOF
 
     sudo chmod +x "$BURP_SCRIPT" || { echo "Failed to make the script executable!"; exit 1; }
@@ -63,7 +63,7 @@ EOF
 
 start_key_generator() {
     print_status "Starting Key Generator..."
-    java -jar "$BURP_DIR/$LOADER_JAR" || { echo "Failed to start the Key Generator!"; exit 1; }
+    java -jar "$BURP_DIR/BurpLoaderKeyGen.jar" || { echo "Failed to start the Key Generator!"; exit 1; }
     print_status "Key Generator process has started. Follow the instructions to generate the key."
 }
 
