5c5e2c0cdb678025b5bdba1decba1e6af8822f36
Sdmrf <syedmarufali23@gmail.com>
2024-08-31 20:27:31 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ download_burpsuite() {
     print_status "Please wait while we complete the process :)"
     local html version download_link
     html=$(curl -s "$BURP_RELEASES_URL")
-    version=$(echo "$html" | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
-
+    
     print_status "Found Burp Suite Version: $version"
+    version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
     download_link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=$version"
 
     wget "$download_link" -O "$BURP_DIR/burpsuite_pro.jar" -q --progress=bar:force || { echo "Download failed!"; exit 1; }
